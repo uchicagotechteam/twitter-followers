2315f79c0cd732c6486d036cd1f21f8bcf276941
Jakub Tucholski <jtucholski00@gmail.com>
2014-05-28 23:38:50 -0500

    adding rudimentary parsing script for hashtags and username

--- parsing.py
+++ parsing.py
@@ -1,7 +1,15 @@
-follower_file = open("follower_lines.json", 'r')
+follower_file = open("currie_followers.json", 'r')
 
 for user in follower_file.readlines():
-    username = user.split(", 'screen_name': ")[1].split(", '")[0]
-    print username
-    location = user.split(", 'location': ")[1].split(", '")[0]
-    print location
+    # checking if user has screen_name attribute
+    if len(user.split("'screen_name'")) > 1:
+        username = user.split("'screen_name'")[1].split(", u'")[0]
+        print username
+    else:
+        print "user has no username"
+    # checking if user has hashtags
+    if len(user.split("'hashtags'")) > 1:
+        hashtags = user.split("'hashtags'")[1].split("], u'")[0]
+        print hashtags
+    else:
+        print "user has no hashtags"
